7988cb7adab8b26bf4c1af0af20a5e5943d61697
Justin O'Halloran <justinohallo@gmail.com>
2019-05-12 14:27:25 -0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports  = {
+    mongoURI: 'mongodb+srv://JustinOhalloran:justin1234@cluster0-vnig8.mongodb.net/test?retryWrites=true'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model 
+
+const Item = require('../../models/Items');
+
+
+module.exports = router ;
