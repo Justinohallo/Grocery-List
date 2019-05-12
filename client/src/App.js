269ae0c1ce80784a9caeb0f6e7de25eab77df4d4
Justin O'Halloran <justinohallo@gmail.com>
2019-05-12 16:39:08 -0700

    updated redux store

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
 
+import {Provider} from "react-redux";
+import store from './store'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+       <div className="App">
     <AppNavbar/>
     <ShoppingList/>
     </div>
+    </Provider>
+   
   );
 }
 
