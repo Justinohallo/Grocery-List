dd0d6b85c50a3ab4e4cd5fed24a1e6104a5da314
Justin O'Halloran <justinohallo@gmail.com>
2019-05-12 18:55:10 -0700

    connected server

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
+import {Container} from 'reactstrap'
+import ItemModal from './components/itemModal'
 
 import {Provider} from "react-redux";
 import store from './store'
@@ -13,7 +15,11 @@ function App() {
     <Provider store={store}>
        <div className="App">
     <AppNavbar/>
+    <Container> 
+    <ItemModal/>
     <ShoppingList/>
+    </Container>
+  
     </div>
     </Provider>
    
