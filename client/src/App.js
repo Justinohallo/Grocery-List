e0fff9537c10b9bf4402e96f5515cf69864392d7
Justin O'Halloran <justinohallo@gmail.com>
2019-05-14 15:33:07 -0700

    added apollo and Currency Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
 import {Container} from 'reactstrap'
-import ItemModal from './components/itemModal'
+import ItemModal from './components/itemModal';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo"
 
 import {Provider} from "react-redux";
 import store from './store'
 
 import './App.css';
 
+const client = new ApolloClient({
+  uri: "http:localhost:5000/graphql"
+})
+
 function App() {
   return (
+    <ApolloProvider client={client}> 
+   
     <Provider store={store}>
        <div className="App">
     <AppNavbar/>
@@ -22,6 +30,7 @@ function App() {
   
     </div>
     </Provider>
+    </ApolloProvider>
    
   );
 }
---
+++ client/src/components/CurrencyCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import gql from "graphql-tag";
+import {Query} from 'react-apollo'
+
+export class CurrencyCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CurrencyCard
